45b668488aac3e44ddc642819b49351d09470c63
Saburov <the.saburchik@gmail.com>
2021-09-22 18:45:20 +0700

    up

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('#board')
 const colors = ['#67E667', '#FFB273', '#FF7373', '#FF7373', '#5CCCCC', '#FFFB73', '#8370D8']
-const SQUARES_NUM = 500
+const SQUARES_NUM = 300
 
 for (let i = 0; i < SQUARES_NUM; i++) {
     const square = document.createElement('div')
